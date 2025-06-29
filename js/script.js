e787c122ae020a99c0a529171b84b53ef8581a8d
VImanTOP <superviman4@gmail.com>
2025-06-29 20:39:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Форма обратной связи
+function validateForm() {
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  if (!name || !email) {
+    alert("Заполните обязательные поля!");
+    return false;
+  }
+  alert("Сообщение отправлено!");
+  return true;
+}
